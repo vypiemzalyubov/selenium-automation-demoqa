ae259a0f8e469cff050c55450566fd9009cd4a5b
Dmitriy Nikolaev <dnikolaev@tradingview.com>
2024-01-25 12:23:53 +0300

    Update conftest and tests

--- conftest.py
+++ conftest.py
@@ -3,8 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-@pytest.fixture(scope="function")
-def driver():
+@pytest.fixture(scope="function", autouse=True)
+def driver(request):
     chrome_options = Options()
     chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument("--ignore-certificate-errors")
@@ -13,5 +13,6 @@ def driver():
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
     chrome_options.add_argument("--user-agent=Selenium")
     driver = webdriver.Chrome(options=chrome_options)
+    request.cls.driver = driver
     yield driver
     driver.quit()
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -7,8 +7,8 @@ from pages.elements_page import ButtonsPage, CheckBoxPage, RadioButtonPage, Text
 
 class TestTextBox:
 
-    def test_text_box(self, driver: WebDriver):
-        text_box_page = TextBoxPage(driver, "https://demoqa.com/text-box")
+    def test_text_box(self):
+        text_box_page = TextBoxPage(self.driver, "https://demoqa.com/text-box")
         text_box_page.open()
         full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
         output_full_name, output_email, output_current_address, output_permanent_address = text_box_page.check_filled_form()
@@ -20,8 +20,8 @@ class TestTextBox:
 
 class TestCheckBox:
 
-    def test_check_box(self, driver: WebDriver):
-        check_box_page = CheckBoxPage(driver, "https://demoqa.com/checkbox")
+    def test_check_box(self):
+        check_box_page = CheckBoxPage(self.driver, "https://demoqa.com/checkbox")
         check_box_page.open()
         check_box_page.open_full_list()
         check_box_page.click_random_checkbox()
@@ -32,8 +32,8 @@ class TestCheckBox:
 
 class TestRadioButton:
 
-    def test_radio_button(self, driver: WebDriver):
-        radio_button_page = RadioButtonPage(driver, "https://demoqa.com/radio-button")
+    def test_radio_button(self):
+        radio_button_page = RadioButtonPage(self.driver, "https://demoqa.com/radio-button")
         radio_button_page.open()
         radio_button_page.click_on_the_radio_button("yes")
         output_yes = radio_button_page.get_output_result()
@@ -48,15 +48,15 @@ class TestRadioButton:
 
 class TestWebTable:
 
-    def test_web_table_person(self, driver: WebDriver):
-        web_table_page = WebTablePage(driver, "https://demoqa.com/webtables")
+    def test_web_table_person(self):
+        web_table_page = WebTablePage(self.driver, "https://demoqa.com/webtables")
         web_table_page.open()
         new_person = web_table_page.add_new_person()
         table_result = web_table_page.check_new_added_person()
         assert new_person in table_result, "The resulting table differs from the expected table"
 
-    def test_web_table_search(self, driver: WebDriver):
-        web_table_page = WebTablePage(driver, "https://demoqa.com/webtables")
+    def test_web_table_search(self):
+        web_table_page = WebTablePage(self.driver, "https://demoqa.com/webtables")
         web_table_page.open()
         key_word = web_table_page.add_new_person()[random.randint(0, 5)]
         time.sleep(5)
@@ -65,8 +65,8 @@ class TestWebTable:
         table_result = web_table_page.check_search_person()
         assert key_word in table_result, "The person was not found in the table"
 
-    def test_web_table_update_person_info(self, driver: WebDriver):
-        web_table_page = WebTablePage(driver, "https://demoqa.com/webtables")
+    def test_web_table_update_person_info(self):
+        web_table_page = WebTablePage(self.driver, "https://demoqa.com/webtables")
         web_table_page.open()
         lastname = web_table_page.add_new_person()[1]
         web_table_page.search_some_person(lastname)
@@ -74,8 +74,8 @@ class TestWebTable:
         row = web_table_page.check_search_person()
         assert age in row, "The person card has not been changed"
 
-    def test_web_table_delete_person(self, driver: WebDriver):
-        web_table_page = WebTablePage(driver, "https://demoqa.com/webtables")
+    def test_web_table_delete_person(self):
+        web_table_page = WebTablePage(self.driver, "https://demoqa.com/webtables")
         web_table_page.open()
         email = web_table_page.add_new_person()[3]
         web_table_page.search_some_person(email)
@@ -83,8 +83,8 @@ class TestWebTable:
         text = web_table_page.check_deleted_person()
         assert text == "No rows found", "The person card has not been deleted"
 
-    def test_web_table_change_count_row(self, driver: WebDriver):
-        web_table_page = WebTablePage(driver, "https://demoqa.com/webtables")
+    def test_web_table_change_count_row(self):
+        web_table_page = WebTablePage(self.driver, "https://demoqa.com/webtables")
         web_table_page.open()
         count = web_table_page.select_rows_count()
         assert count == [5, 10, 20, 50, 100], "The number of rows in the table has not been changed or has not been chahged incorrectly"
@@ -93,7 +93,7 @@ class TestWebTable:
 class TestButtonPage:
 
     def test_different_click_on_the_buttons(self, driver):
-        button_page = ButtonsPage(driver, "https://demoqa.com/buttons")
+        button_page = ButtonsPage(self.driver, "https://demoqa.com/buttons")
         button_page.open()
         double = button_page.click_on_different_button("double")
         right = button_page.click_on_different_button("right")
