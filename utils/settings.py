0a8579ad51163fbd1a7af7b5825b0bb03fd277d0
Dmitriy Nikolaev <dnikolaev@tradingview.com>
2024-01-31 17:23:51 +0300

    Change settings.py

--- utils/settings.py
+++ utils/settings.py
@@ -4,15 +4,16 @@ from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv(".env.example"))
 
 
-class Settings(BaseSettings):
-    BASE_PAGE: str
+class UISettings(BaseSettings):
+    base_page: str
 
     @property
     def base_page(self):
-        return self.BASE_PAGE
+        return self.base_page
 
     class ConfigDict:
         env_file = ".env.example"
+        env_file_encoding = "utf-8"
 
 
-settings = Settings()
+settings = UISettings()
