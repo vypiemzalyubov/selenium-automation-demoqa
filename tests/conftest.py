7a3602fdd4d239968020a7fa0a4f9d8e50b25418
Dmitriy Nikolaev <dnikolaev@tradingview.com>
2024-02-20 12:23:10 +0300

    Change conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,26 +14,26 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope='function', autouse=True)
 def driver(request: pytest.FixtureRequest):
-    chrome_option = Options()
-    chrome_option.page_load_strategy = 'eager'
-    chrome_option.add_argument('--ignore-certificate-errors')
-    chrome_option.add_argument('--no-sandbox')
-    chrome_option.add_argument('--disable-dev-shm-usage')
-    chrome_option.add_argument('--disable-blink-features=AutomationControlled')
-    chrome_option.add_argument('--user-agent=Selenium')
+    chrome_options = Options()
+    chrome_options.page_load_strategy = 'eager'
+    chrome_options.add_argument('--ignore-certificate-errors')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+    chrome_options.add_argument('--user-agent=Selenium')
 
     headless = request.config.getoption('--headless')
     if headless == 'true':
-        chrome_option.add_argument('--headless')
+        chrome_options.add_argument('--headless')
 
     window_size = request.config.getoption('--window-size')
     if window_size == 'max':
-        chrome_option.add_argument('--start-maximized')
+        chrome_options.add_argument('--start-maximized')
     else:
         width, height = window_size.split(',')
-        chrome_option.add_argument(f'--window-size={width},{height}')
+        chrome_options.add_argument(f'--window-size={width},{height}')
 
-    driver = webdriver.Chrome(options=chrome_option)
+    driver = webdriver.Chrome(options=chrome_options)
     request.cls.driver = driver
     yield driver
     driver.quit()
